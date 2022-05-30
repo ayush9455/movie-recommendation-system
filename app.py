bbdfdae63bbbb784465020a956aa4521c1832506
ayush9455 <ayushgupta.ak@gmail.com>
2022-05-30 12:17:16 +0530

    wide mode & 12movies

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import requests
-
 latent_matrix_df = pickle.load(open('content.pkl','rb'))
 latent_matrix_2_df = pickle.load(open('collabrative.pkl','rb'))
 link = pickle.load(open('link.pkl','rb'))
@@ -28,15 +27,15 @@ def recommend(movie):
     similar.sort_values('hybrid', ascending=False, inplace=True)
     recommended_movie_names = []
     recommended_movie_posters = []
-    for i in similar[1:10].index:
+    for i in similar[1:13].index:
         index = link[link['title'] == i].index[0]
         id=link.iloc[index]['tmdbId']
         recommended_movie_posters.append(fetch_poster(id))
         recommended_movie_names.append(i)
     return recommended_movie_names,recommended_movie_posters
 
-st.set_page_config(page_title='Movie Recommendation System')
-st.title('Movie Recommender System')
+st.set_page_config(page_title='Movie Recommendation System',layout="wide")
+st.title('Movie Recommendation System')
 st.image('image.png')
 movie_list = link['title']
 selected_movie = st.selectbox(
@@ -48,6 +47,8 @@ if st.button('Show Recommendations'):
         recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
         col1, col2, col3, col4 = st.columns(4)
         col5, col6, col7, col8 = st.columns(4)
+        col9, col10, col11, col12 = st.columns(4)
+
         with col1:
             st.image(recommended_movie_posters[0])
             st.text(recommended_movie_names[0])
@@ -70,8 +71,21 @@ if st.button('Show Recommendations'):
             st.image(recommended_movie_posters[6])
             st.text(recommended_movie_names[6])
         with col8:
+            st.image(recommended_movie_posters[7])
+            st.text(recommended_movie_names[7])
+        with col9:
             st.image(recommended_movie_posters[8])
             st.text(recommended_movie_names[8])
+        with col10:
+            st.image(recommended_movie_posters[9])
+            st.text(recommended_movie_names[9])
+        with col11:
+            st.image(recommended_movie_posters[10])
+            st.text(recommended_movie_names[10])
+        with col12:
+            st.image(recommended_movie_posters[11])
+            st.text(recommended_movie_names[11])
+        st.subheader("Made By AYUSH GUPTA (ayushgupta.ak@gmail.com)")
 hide_menu_style="""
     <style>
     #MainMenu{visibility :hidden;}
