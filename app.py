389c1da39334aa904fd927be87440a5b5f7ef6bb
ayush9455 <ayushgupta.ak@gmail.com>
2022-06-02 15:05:24 +0530

    trailer link added

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import requests
+import webbrowser
 
 latent_matrix_df = pickle.load(open('content.pkl','rb'))
 latent_matrix_2_df = pickle.load(open('collabrative.pkl','rb'))
@@ -16,6 +17,14 @@ def fetch_poster(movie_id):
     full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
     return full_path
 
+def fetch_trailer(movie_id):
+    url = "http://api.themoviedb.org/3/movie/{}/videos?api_key=8265bd1679663a7ea12ac168da84d2e8".format(movie_id)
+    data = requests.get(url)
+    data = data.json()
+    a = data['results']
+    yt_path= a[0]['key']
+    full_path = "https://www.youtube.com/watch?v=" + yt_path
+    return full_path
 def recommend(movie):
     from sklearn.metrics.pairwise import cosine_similarity
     a_1 = np.array(latent_matrix_df.loc[movie]).reshape(1, -1)
@@ -28,15 +37,17 @@ def recommend(movie):
     similar.sort_values('hybrid', ascending=False, inplace=True)
     recommended_movie_names = []
     recommended_movie_posters = []
-    for i in similar[1:10].index:
+    recommended_movie_trailer=[]
+    for i in similar[1:13].index:
         index = link[link['title'] == i].index[0]
         id=link.iloc[index]['tmdbId']
         recommended_movie_posters.append(fetch_poster(id))
         recommended_movie_names.append(i)
-    return recommended_movie_names,recommended_movie_posters
+        recommended_movie_trailer.append(fetch_trailer(id))
+    return recommended_movie_names,recommended_movie_posters,recommended_movie_trailer
 
-st.set_page_config(page_title='Movie Recommendation System')
-st.title('Movie Recommender System')
+st.set_page_config(page_title='Movie Recommendation System',layout="wide")
+st.title('Movie Recommendation System')
 st.image('image.png')
 movie_list = link['title']
 selected_movie = st.selectbox(
@@ -45,33 +56,60 @@ selected_movie = st.selectbox(
 )
 if st.button('Show Recommendations'):
     with st.spinner(text='Loading !!!'):
-        recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
+        recommended_movie_names,recommended_movie_posters,recommended_movie_trailer = recommend(selected_movie)
         col1, col2, col3, col4 = st.columns(4)
         col5, col6, col7, col8 = st.columns(4)
+        col9, col10, col11, col12 = st.columns(4)
+
         with col1:
             st.image(recommended_movie_posters[0])
             st.text(recommended_movie_names[0])
+            st.write("[Watch Trailer](%s)" %recommended_movie_trailer[0])
         with col2:
             st.image(recommended_movie_posters[1])
             st.text(recommended_movie_names[1])
+            st.write("[Watch Trailer](%s)" %recommended_movie_trailer[1])
         with col3:
             st.image(recommended_movie_posters[2])
             st.text(recommended_movie_names[2])
+            st.write("[Watch Trailer](%s)" %recommended_movie_trailer[2])
         with col4:
             st.image(recommended_movie_posters[3])
             st.text(recommended_movie_names[3])
+            st.write("[Watch Trailer](%s)" %recommended_movie_trailer[3])
         with col5:
             st.image(recommended_movie_posters[4])
             st.text(recommended_movie_names[4])
+            st.write("[Watch Trailer](%s)" %recommended_movie_trailer[4])
         with col6:
             st.image(recommended_movie_posters[5])
             st.text(recommended_movie_names[5])
+            st.write("[Watch Trailer](%s)" %recommended_movie_trailer[5])
         with col7:
             st.image(recommended_movie_posters[6])
             st.text(recommended_movie_names[6])
+            st.write("[Watch Trailer](%s)" %recommended_movie_trailer[6])
         with col8:
+            st.image(recommended_movie_posters[7])
+            st.text(recommended_movie_names[7])
+            st.write("[Watch Trailer](%s)" %recommended_movie_trailer[7])
+        with col9:
             st.image(recommended_movie_posters[8])
             st.text(recommended_movie_names[8])
+            st.write("[Watch Trailer](%s)" %recommended_movie_trailer[8])
+        with col10:
+            st.image(recommended_movie_posters[9])
+            st.text(recommended_movie_names[9])
+            st.write("[Watch Trailer](%s)" %recommended_movie_trailer[9])
+        with col11:
+            st.image(recommended_movie_posters[10])
+            st.text(recommended_movie_names[10])
+            st.write("[Watch Trailer](%s)" %recommended_movie_trailer[10])
+        with col12:
+            st.image(recommended_movie_posters[11])
+            st.text(recommended_movie_names[11])
+            st.write("[Watch Trailer](%s)" %recommended_movie_trailer[11])
+        st.subheader("Made By AYUSH GUPTA (ayushgupta.ak@gmail.com)")
 hide_menu_style="""
     <style>
     #MainMenu{visibility :hidden;}
---
+++ readme.txt
@@ -0,0 +1,6 @@
+
+-- ratings_f2=ratings_f1.pivot(index='movieId',columns='userId',values='rating').fillna(0) ---
+This Line Of Code in Algorithm.ipynb requires approxiamtely 16GB of Memory
+
+--- WEBAPP IS DEPLOYED AT THE FOLLOWING LINK : ---
+   https://ag-movie-recommender.herokuapp.com/
